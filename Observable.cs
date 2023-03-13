4c7df10576371b6c8e08b89dfb966dc55b897018
student <student@B313-3>
2023-03-13 12:01:30 +0700

    Добавленны новые классы

---
+++ Observable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ7
+{
+    interface Observable
+    {
+        void AddOvserver();
+        void RemoveOvserver();
+
+    }
+}
